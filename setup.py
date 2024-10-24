a0ac0e1655f3f44c022c0132598fd9d58e522d2c
abdorado1984 <abdallahradwan2011@gmail.com>
2024-10-24 10:24:06 +0300

    My Python Package
    First Commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name='AbdallahRadwanLib',
+    version='0.1',
+    packages=['arUtilities'],
+    url='https://github.com/abdorado1984/AbdallahPackage',
+    license='MIT',
+    author='Abdallah Radwan',
+    author_email='AbdallahRadwan2011@gmail.com',
+    description='My First Package for Python Projects with PyPI Package - October 2024',  
+)

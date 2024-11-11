8357e564b914ff4a784cc8ad73806dc1e6c61042
abdorado1984 <abdallahradwan2011@gmail.com>
2024-11-11 14:19:49 +0300

    update my package with version 1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='AbdallahRadwanLib',
-    version='1.3',    
+    version='1.4',    
     description='My First Package for Python Projects with PyPI Package - October 2024',  
     # package_dir={"": "app"},
     # packages=find_packages(where="app"),
